7008dc3f7843e51bd66c4804376a59ae7601ec65
roney ruiz <roneyruizrojas@gmail.com>
2025-01-02 13:54:02 -0600

    feat(client): install and setup tailwind

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,6 @@
 export const Home = (): JSX.Element => {
   return (
-    <div className="p-10">
+    <div className="h-screen w-screen p-10 bg-[#0b1120] text-white">
       <h1 className="text-3xl font-semibold">Home Page</h1>
     </div>
   );

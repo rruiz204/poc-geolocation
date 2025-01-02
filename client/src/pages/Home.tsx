58c3a389f4faa403c0315eb37288a3f46fd7b578
roney ruiz <roneyruizrojas@gmail.com>
2025-01-02 15:05:23 -0600

    feat(client): connecting to socket

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,4 +1,17 @@
+import { useEffect } from "react";
+import { useSocket } from "../sockets/useSocket";
+
 export const Home = (): JSX.Element => {
+  const { connect, disconnect } = useSocket();
+
+  useEffect(() => {
+    connect("http://localhost:6000/coords");
+
+    console.log("successfully connected to the server");
+
+    return () => {disconnect()};
+  }, []);
+
   return (
     <div className="h-screen w-screen p-10 bg-[#0b1120] text-white">
       <h1 className="text-3xl font-semibold">Home Page</h1>

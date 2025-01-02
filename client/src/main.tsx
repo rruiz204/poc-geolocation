e0c9c0f6f2c5af507db90e9a44c9fcb7c1f8ccf7
roney ruiz <roneyruizrojas@gmail.com>
2025-01-02 13:50:17 -0600

    feat(client): configure react router

---
+++ client/src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+
+import { Router } from "./router";
+import "./index.css"
+
+const root = document.getElementById("root")!;
+
+const component = (
+  <StrictMode>
+    <Router></Router>
+  </StrictMode>
+);
+
+createRoot(root).render(component);
---
+++ client/src/router.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+import { Home } from "./pages/Home";
+
+export const Router = (): JSX.Element => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home></Home>} />
+      </Routes>
+    </BrowserRouter>
+  );
+};

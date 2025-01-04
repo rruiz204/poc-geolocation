5a9506c707f24393e12ea7ac763ad1adf870e398
roney ruiz <roneyruizrojas@gmail.com>
2025-01-04 17:08:50 -0600

    refactor(server): change ping rest response

--- server/Src/Controllers/PingController.cs
+++ server/Src/Controllers/PingController.cs
@@ -1,14 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
 
 namespace Server.Src.Controllers;
 
 [ApiController]
 [Route("api/")]
+[EnableRateLimiting("FixedWindows")]
 public class PingController : ControllerBase
 {
   [HttpGet("ping-rest")]
   public IActionResult Ping()
   {
-    return Ok("pong");
+    return Ok(new { ping = "pong" });
   }
 }

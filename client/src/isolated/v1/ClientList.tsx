0e81ed9ffe70d496420c4a0b1076afda980360ce
roney ruiz <roneyruizrojas@gmail.com>
2025-01-06 13:59:57 -0600

    feat(client): our best option in this moment

---
+++ client/src/isolated/v1/ClientList.tsx
@@ -0,0 +1,20 @@
+import { Client } from "../../models/Client";
+
+interface Props {
+  clients: Client[];
+};
+
+export const ClientList = ({ clients }: Props): JSX.Element => {
+  return (
+    <div className="flex flex-col gap-y-3">
+      {
+        clients.map((client: Client) => (
+          <div key={client.name} className="flex flex-col items-center gap-y-2">
+            <p>{client.name}</p>
+            <p>{client.latitude}, {client.longitude}</p>
+          </div>
+        ))
+      }
+    </div>
+  );
+};
--- client/src/router.tsx
+++ client/src/router.tsx
@@ -4,6 +4,7 @@ import { Home } from "./pages/Home";
 import { PingWs } from "./pages/PingWs";
 import { PingRest } from "./pages/PingRest";
 import { Coords } from "./pages/Coords";
+import { FirstIsolated } from "./isolated/v1/FirstIsolated";
 
 export const Router = (): JSX.Element => {
   return (
@@ -13,6 +14,8 @@ export const Router = (): JSX.Element => {
         <Route path="/ping-ws" element={<PingWs></PingWs>} />
         <Route path="/ping-rest" element={<PingRest></PingRest>} />
         <Route path="/coords" element={<Coords></Coords>} />
+        
+        <Route path="/isolated/v1" element={<FirstIsolated></FirstIsolated>} />
       </Routes>
     </BrowserRouter>
   );
